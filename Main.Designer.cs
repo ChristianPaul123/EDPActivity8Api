d39c8861675b564947042d0860abc7b2b99fb226
ChristianPaul123 <christianpaulespares1@gmail.com>
2024-05-30 17:24:49 +0800

    Add project files.

---
+++ Main.Designer.cs
@@ -0,0 +1,251 @@
+﻿
+namespace CsharpApi
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.Display = new System.Windows.Forms.TextBox();
+            this.btnGet = new System.Windows.Forms.Button();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.Quantity_box = new System.Windows.Forms.TextBox();
+            this.Published_box = new System.Windows.Forms.TextBox();
+            this.Number_box = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.btnPost = new System.Windows.Forms.Button();
+            this.Category_box = new System.Windows.Forms.TextBox();
+            this.Author_box = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.Title_box = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.Display);
+            this.groupBox1.Controls.Add(this.btnGet);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(742, 264);
+            this.groupBox1.TabIndex = 5;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Enter += new System.EventHandler(this.groupBox1_Enter);
+            // 
+            // Display
+            // 
+            this.Display.Location = new System.Drawing.Point(26, 21);
+            this.Display.Multiline = true;
+            this.Display.Name = "Display";
+            this.Display.Size = new System.Drawing.Size(699, 134);
+            this.Display.TabIndex = 4;
+            // 
+            // btnGet
+            // 
+            this.btnGet.Location = new System.Drawing.Point(136, 182);
+            this.btnGet.Name = "btnGet";
+            this.btnGet.Size = new System.Drawing.Size(496, 57);
+            this.btnGet.TabIndex = 3;
+            this.btnGet.Text = "GET";
+            this.btnGet.UseVisualStyleBackColor = true;
+            this.btnGet.Click += new System.EventHandler(this.btnGet_Click_1);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.Quantity_box);
+            this.groupBox2.Controls.Add(this.Published_box);
+            this.groupBox2.Controls.Add(this.Number_box);
+            this.groupBox2.Controls.Add(this.label6);
+            this.groupBox2.Controls.Add(this.label5);
+            this.groupBox2.Controls.Add(this.label4);
+            this.groupBox2.Controls.Add(this.btnPost);
+            this.groupBox2.Controls.Add(this.Category_box);
+            this.groupBox2.Controls.Add(this.Author_box);
+            this.groupBox2.Controls.Add(this.label3);
+            this.groupBox2.Controls.Add(this.label2);
+            this.groupBox2.Controls.Add(this.Title_box);
+            this.groupBox2.Controls.Add(this.label1);
+            this.groupBox2.Location = new System.Drawing.Point(13, 292);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(741, 245);
+            this.groupBox2.TabIndex = 6;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Enter += new System.EventHandler(this.groupBox2_Enter);
+            // 
+            // Quantity_box
+            // 
+            this.Quantity_box.Location = new System.Drawing.Point(466, 120);
+            this.Quantity_box.Name = "Quantity_box";
+            this.Quantity_box.Size = new System.Drawing.Size(228, 22);
+            this.Quantity_box.TabIndex = 12;
+            // 
+            // Published_box
+            // 
+            this.Published_box.Location = new System.Drawing.Point(465, 88);
+            this.Published_box.Name = "Published_box";
+            this.Published_box.Size = new System.Drawing.Size(228, 22);
+            this.Published_box.TabIndex = 11;
+            // 
+            // Number_box
+            // 
+            this.Number_box.Location = new System.Drawing.Point(465, 48);
+            this.Number_box.Name = "Number_box";
+            this.Number_box.Size = new System.Drawing.Size(228, 22);
+            this.Number_box.TabIndex = 10;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(359, 123);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(101, 17);
+            this.label6.TabIndex = 9;
+            this.label6.Text = "Book Quantity:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(359, 88);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(109, 17);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Book published:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(359, 48);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(96, 17);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Book number:";
+            // 
+            // btnPost
+            // 
+            this.btnPost.Location = new System.Drawing.Point(135, 168);
+            this.btnPost.Name = "btnPost";
+            this.btnPost.Size = new System.Drawing.Size(484, 57);
+            this.btnPost.TabIndex = 6;
+            this.btnPost.Text = "POST";
+            this.btnPost.UseVisualStyleBackColor = true;
+            this.btnPost.Click += new System.EventHandler(this.btnPost_Click_1);
+            // 
+            // Category_box
+            // 
+            this.Category_box.Location = new System.Drawing.Point(110, 123);
+            this.Category_box.Name = "Category_box";
+            this.Category_box.Size = new System.Drawing.Size(232, 22);
+            this.Category_box.TabIndex = 5;
+            // 
+            // Author_box
+            // 
+            this.Author_box.Location = new System.Drawing.Point(110, 88);
+            this.Author_box.Name = "Author_box";
+            this.Author_box.Size = new System.Drawing.Size(232, 22);
+            this.Author_box.TabIndex = 4;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(5, 123);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(103, 17);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Book category:";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(5, 88);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(90, 17);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Book Author:";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // Title_box
+            // 
+            this.Title_box.Location = new System.Drawing.Point(110, 45);
+            this.Title_box.Name = "Title_box";
+            this.Title_box.Size = new System.Drawing.Size(232, 22);
+            this.Title_box.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 48);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(75, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Book Title:";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(782, 553);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.groupBox2);
+            this.Name = "Form2";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "C#-MYSQL API By Christian Paul Espares";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox Display;
+        private System.Windows.Forms.Button btnGet;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button btnPost;
+        private System.Windows.Forms.TextBox Category_box;
+        private System.Windows.Forms.TextBox Author_box;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox Title_box;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox Number_box;
+        private System.Windows.Forms.TextBox Quantity_box;
+        private System.Windows.Forms.TextBox Published_box;
+    }
+}
---
+++ Main.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+
+
+namespace CsharpApi
+{
+    public partial class Main : Form
+    {
+        private static readonly HttpClient client = new HttpClient();
+
+        public Main()
+        {
+            InitializeComponent();
+        }
+
+        private async void btnGet_Click_1(object sender, EventArgs e)
+        {
+            try
+            {
+                Display.Clear();
+                HttpResponseMessage response = await client.GetAsync("http://localhost/myphpapi/api.php");
+                response.EnsureSuccessStatusCode();
+                string responseBody = await response.Content.ReadAsStringAsync();
+                Display.Text = responseBody;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error: " + ex.Message);
+            }
+        }
+
+        private async void btnPost_Click_1(object sender, EventArgs e)
+        {
+            var newBook = new
+            {
+                book_title = Title_box.Text, // Example field, use txtBookTitle.Text if you have that field
+                book_author = Author_box.Text, // Example author_id
+                book_category = Category_box.Text, // Example category_id
+                book_number = Number_box.Text, // Example book number
+                book_published = Published_box.Text, // Example published date
+                book_quantity = Quantity_box.Text, // Example quantity
+                date_added = DateTime.Now.ToString("yyyy-MM-dd") // Example date added
+            };
+            string json = JsonConvert.SerializeObject(newBook);
+            HttpContent content = new StringContent(json, Encoding.UTF8, "application/json");
+
+            try
+            {
+                HttpResponseMessage response = await client.PostAsync("http://localhost/myphpapi/api.php", content);
+                response.EnsureSuccessStatusCode();
+                string responseBody = await response.Content.ReadAsStringAsync();
+                Display.Text = responseBody;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error: " + ex.Message);
+            }
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+
+        private async void btnPost_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void groupBox2_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+    }
+}
